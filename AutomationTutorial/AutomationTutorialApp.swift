ae72ed0f8f5574b7a5d79ee868dc60d19ee7b8e6
Andrii Kvashuk <andremtcp2015a@gmail.com>
2024-07-11 12:28:13 +0100

    Initial commit

---
+++ AutomationTutorial/AutomationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomationTutorialApp.swift
+//  AutomationTutorial
+//
+//  Created by Andrii Kvashuk on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AutomationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutomationTutorialUITests/AutomationTutorialUITests.swift
@@ -0,0 +1,15 @@
+//
+//  AutomationTutorialUITests.swift
+//  AutomationTutorialUITests
+//
+//  Created by Andrii Kvashuk on 11/07/2024.
+//
+
+import XCTest
+
+final class AutomationTutorialUITests: XCTestCase {
+
+    func test_addittion() {
+        XCTAssertEqual(2*2, 4)
+    }
+}
